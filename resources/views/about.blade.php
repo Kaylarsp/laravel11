fa38d6dd5aa16011a4b0a834bf4a7e8640a6184b
Kayla <satitiputri.kayla@gmail.com>
2025-01-20 14:46:07 +0700

    add Blade components for better reusability

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,19 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>About Page</title>
-    <link rel="stylesheet" href="css/style.css">
-</head>
-
-<body>
-    <a href="/">Home</a>
-    <a href="/about">About</a>
-    <h1>Halaman About</h1>
-    <h3>Owner: {{ $nama }} </h3> <!-- itu sama aja spt tag php -->
-    <img src="img/pooh.png" alt="pooh" width="300" >
-</body>
-
-</html>
+<x-layout>
+    <x-slot:title> {{$title}} </x-slot>
+    <h3 class="text-xl">Ini adalah halaman about</h3>
+    <p>Heloo my name is {{$nama}} </p>
+</x-layout>
